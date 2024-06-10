820c99a2b968107e29d1a7ffe1fb5cbc57950a8d
nikonba777 <164557496+nikonba777@users.noreply.github.com>
2024-06-11 01:47:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html><html lang="ru"><head>
+<noscript><div><img src="https://mc.yandex.ru/watch/69418195" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
+    <title>1000 тыс каналов!</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Мастерская Сочи-Телемастер">
+    <meta name="keywords" content="ремонт телевизоров">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="all">
+    <link rel="canonical" href="https://sochi-telemaster.ru/">
+    <meta property="og:title" content="Настройка тв удаленно !!!">
+    <meta property="og:description" content="Мастерская Сочи-Телемастер">
+    <meta property="og:image" content="images/favicon-512x512.png">
+    <meta property="og:url" content="https://sochi-telemaster.ru">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"><!-- helps IE -->
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon-180x180.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon-192x192.png">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/favicons/favicon-144x144.png">
+    <meta name="msapplication-config" content="/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="manifest" href="/manifest.json">
+
+    <link rel="stylesheet" type="text/css" href="css/tv.css">
+
+</head>
+<body>
+
+<header>
+    <section>
+        <head-logo><a href="./index.php" title="Сочи-Телемастер"><img src="images/favicon-180x180.png" alt="       "></a></head-logo>
+        <head-title><h1>5000 тыс каналов за 2 $ в месяц !!! </h1></head-title>
+        <head-tel>
+
+            <popup-block>
+                <!-- Для кождого всплывающего окна нужна уникальная подстрока "popup_button_header" -->
+                <input class="popup-input-open" type="radio" name="popup_button_header" id="popup_button_header-open" value="Оставить заявку">
+                <popup-block-background>
+                    <label for="popup_button_header-close"></label>
+                    <popup-block-overlay>
+                        <input class="popup-input-close" type="radio" name="popup_button_header" id="popup_button_header-close" value="Закрыть">
+                        <div>
+                            <popup-block-area>
+                                <h3>Оставить заявку</h3>
+                                <form method="POST" action="./">
+                                <label>Для подключения каналов введите <br><input type="text" name="name" placeholder="Модель вашего телевизора ?" required=""></label>
+                                <label>Ваш номер телефона WhatsApp: <br><input type="tel" name="phone" placeholder="Ваш номер тел WhatsApp ?" required=""></label>
+                                    <button type="submit" name="button" value="Кнопка в шапке">Отправить</button>
+                                </form>
+                            </popup-block-area>
+                        </div>
+                    </popup-block-overlay>
+                </popup-block-background>
+            </popup-block>
+        </head-tel>
+    </section>
+</header>
+
+<main>
+
+<div-slider class="slider_background">
+    <div-banner>
+        <h2>VIP ТВ ПАКЕТ !</h2>
+		
+		<footer>
+    <div>
+
+   
+    </div>
+    <div>
+
+    </div>
+</footer>
+        <span-action>5000 ТЫС КАНАЛОВ НА РУССКОМ ЯЗЫКЕ </span-action><br>
+        <span-promo> <b>Удаленное подключение за 5минут !</b></span-promo><br>
+        <span>
+Большое количество популярных  каналов за минимальную абонплату <b></b> помощь в подключении,оплата после пдключения!
+<b></b>Обзор списка каналов на Ютуб !
+
+<b> </b>  <br>
+<b> </b>  <br>
+
+
+<iframe width="300" height="215" src="https://www.youtube.com/embed/z7C5y0MXmUY" frameborder="0" allowfullscreen></iframe>
+
+
+
+<b> </b>  <br>
+<b>   </b>
+        </span>
+        <popup-block>
+            <!-- Для кождого всплывающего окна нужна уникальная подстрока "popup_button_main" -->
+            <input class="popup-input-open" type="radio" name="popup_button_main" id="popup_button_main-open" value="Оставить заявку">
+            <popup-block-background>
+                <label for="popup_button_main-close"></label>
+                <popup-block-overlay>
+                    <input class="popup-input-close" type="radio" name="popup_button_main" id="popup_button_main-close" value="Закрыть">
+                    <div>
+                        <popup-block-area>
+                            <h3>Оставить заявку</h3>
+                            <form method="POST" action="./">
+                                <label>Для подключения каналов введите <br><input type="text" name="name" placeholder="Модель вашего телевизора ?" required=""></label>
+                                <label>Ваш номер телефона WhatsApp: <br><input type="tel" name="phone" placeholder="Ваш номер тел WhatsApp ?" required=""></label>
+                                <button type="submit" name="button" value="Кнопка на заставке">Отправить</button>
+                            </form>
+                        </popup-block-area>
+                    </div>
+                </popup-block-overlay>
+            </popup-block-background>
+        </popup-block>
+        <br><br>
+    </div-banner>
+</div-slider>
+
+<section id="services">
+    <h2>Услуги и цены</h2>
+    <div-services>
+        <div-service>
+            <h3>Удаленное подключение  тв  </h3>
+            <img src="images/s-2.jpg" alt="Ремонтом телевизоров" loading="lazy">
+            <p>Удаленное подключение смарт тв</p>
+            <span>  <b>0 руб(0 $)</b>   </span>
+        </div-service>
+        <div-service>
+            <h3>Удаленное подключение  приставок ! </h3>
+            <img src="images/s-1.jpg" alt="Установка приставок интернетТВ" loading="lazy">
+            <p>Настройка смарт  приставки !</p>
+            <span>  <b>0 руб  (0 $) </b>   </span>
+        </div-service>
+        <div-service>
+            <h3>Бесплатная удаленная поддержка</h3>
+            <img src="images/s-3.jpg" alt="Установка антенн" loading="lazy">
+            <p>Удаленная поддержка на весь срок просмотра тв пакета !</p>
+            <span>  <b> 0 руб  (0 $)</b>  </span>
+        </div-service>
+        <div-service>
+
+        </div-service>
+    </div-services>
+</section>
+
+
+
+    <h2></h2>
+    <div-contacts>
+        <h4>   </h4>
+
+        <popup-block>
+            <!-- Для кождого всплывающего окна нужна уникальная подстрока "popup_button_footer" -->
+            <input class="popup-input-open" type="radio" name="popup_button_footer" id="popup_button_footer-open" value="Оставить заявку">
+            <popup-block-background>
+                <label for="popup_button_footer-close"></label>
+                <popup-block-overlay>
+                    <input class="popup-input-close" type="radio" name="popup_button_footer" id="popup_button_footer-close" value="Закрыть">
+                    <div>
+                        <popup-block-area>
+                            <h3>Оставить заявку</h3>
+                            <form method="POST" action="./">
+                                <label>Для подключения каналов введите<br><input type="text" name="name" placeholder="Модель вашего телевизора ?" required=""></label>
+                                <label>Ваш номер телефона WhatsApp: <br><input type="tel" name="phone" placeholder="Ваш номер тел WhatsApp ?" required="" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"></label>
+                                <button type="submit" name="button" value="Кнопка в подвале">Отправить</button>
+                            </form>
+                        </popup-block-area>
+                    </div>
+                </popup-block-overlay>
+            </popup-block-background>
+        </popup-block>
+        <br><br>
+
+    </div-contacts>
+</section>
+
+</main>
+
+
+
+<center><a href="/atv" title="Каталог приложений"></a></center>
+
+<br>
+
+<script async="" src="js/tag.js"></script><script>
+// alert('No');
+</script>
+
+<script src="js/tv.js"></script>
+
+<!-- Yandex.Metrika counter -->
+
+<!-- /Yandex.Metrika counter -->
+
+<!-- Begin LeadBack code {literal} -->
+<script defer="">
+
+</script>
+<!-- End LeadBack code {/literal} -->
+
+
+
+
+</body></html>
+
+<?php
+// Проверка, что была отправлена заявка
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    // Доступы к телеграм
+    define('TELEGRAM_TOKEN', '1609438136:AAEPKNgdXKM43bv3dqzumsLPaiAtV1TVvSw');
+
+    // сюда нужно вписать ваш внутренний айдишник
+    define('TELEGRAM_CHATID', '210679475');
+
+    function message_to_telegram($text) {
+        $ch = curl_init();
+        curl_setopt_array(
+            $ch,
+            array(
+                CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
+                CURLOPT_POST => TRUE,
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_POSTFIELDS => array(
+                    'chat_id' => TELEGRAM_CHATID,
+                    'text' => $text,
+                ),
+            )
+        );
+        curl_exec($ch);
+    }
+    
+
+    message_to_telegram("Новая заявка:\n ".$_POST['phone']."\n ".$_POST['name']."\n ".$_POST['tv']."\n ".$_POST['ne']);
+}
+?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+?>
+<!--
+Всплывающее окно при удачной отправке заявки
+-->
+<div id="isok" style="text-align: center;">
+    <popup-block>
+<!--         <input class="popup-input-open" type="radio" name="popup-button-ok" value="Оставить заявку" style=""> -->
+        <popup-block-background>
+            <label for="popup-button-ok" onclick="hideok()"></label>
+            <popup-block-overlay>
+                <input class="popup-input-close" type="radio" checked name="popup-button-ok" id="popup-button-ok" value="x" onclick="hideok()">
+                <div>
+                    <popup-block-area>
+                        <form method="post" action="/">
+                            <fieldset>
+                            <legend>Спасибо, ваша заявка принята, в ближайшее время с вами свяжется наш мастер!!!</legend>
+                        </form>
+                    </popup-block-area>
+                </div>
+            </popup-block-overlay>
+        </popup-block-background>
+    </popup-block>
+</div>
+<script>
+function hideok() {
+    document.querySelector('#isok').remove();
+    location="/" 
+}
+</script>
+<!-- <input form="send" name="ok" hidden="" type="text" value="ok"> -->
+<?php
+}
+?>
+
+
+
